8a6949cb76adf41b816b7bdb264f15ff0d801e88
Gilberto Junior <gjunior@ciandt.com>
2023-01-12 13:26:57 -0300

    feat: add Coming Soon screen

---
+++ Netflix Clone/ViewModels/TitleViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  TitleViewModel.swift
+//  Netflix Clone
+//
+//  Created by Gilberto Junior on 12/01/23.
+//
+
+import Foundation
+
+struct TitleViewModel {
+    let titleName: String
+    let posterURL: String
+}
